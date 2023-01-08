8aba7d38c65a8b277b6e750535c863b1fcb972a8
Leonid Vasilev <vasilev.l@panoramik.ru>
2023-01-08 17:43:37 +0300

    added propses to terrain

---
+++ src/Imp/Assets/Scripts/Terrain/ActiveSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Imp
+{
+    public class ActiveSwitcher : MonoBehaviour
+    {
+        [SerializeField] private GameObject _observableGameObject;
+
+        private void Update()
+        {
+            gameObject.SetActive(!_observableGameObject.activeInHierarchy);
+        }
+    }
+}
