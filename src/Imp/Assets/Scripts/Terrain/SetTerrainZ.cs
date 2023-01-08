33a0048c3b3e4280ad9efc36d36da7b85ca01680
Leonid Vasilev <vasilev.l@panoramik.ru>
2023-01-08 17:54:50 +0300

    visual fixes

--- src/Imp/Assets/Scripts/Terrain/SetTerrainZ.cs
+++ src/Imp/Assets/Scripts/Terrain/SetTerrainZ.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 namespace Imp
 {
-    public class SetTerrainY : MonoBehaviour
+    public class SetTerrainZ : MonoBehaviour
     {
-        void Start()
+        [SerializeField] private float _additionalShift;
+        
+        private void Start()
         {
             int childCount = transform.childCount;
             for (int i = 0; i < childCount; i++)
             {
                 Transform childTransform = transform.GetChild(i);
                 Vector3 position = childTransform.position;
-                position.z = position.y;
+                position.z = position.y + _additionalShift;
                 childTransform.position = position;
             }
         }
